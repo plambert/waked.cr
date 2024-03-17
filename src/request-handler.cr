5c21b1f2407a6768fa9002b07fa20b99d923281f
Paul M. Lambert <plambert@plambert.net>
2024-03-17 12:17:31 -0700

    fix output handling

--- src/request-handler.cr
+++ src/request-handler.cr
@@ -11,20 +11,31 @@ class Wake::RequestHandler
 
   def call(context)
     if context.request.path =~ %r{^/(\w+)$}
+      context.response.content_type = "text/plain"
       if mac_addr = @devices[$1]?
         output = IO::Memory.new
         error = IO::Memory.new
         status = Process.run command: "sudo", args: ["etherwake", "-i", @interface, "-D", mac_addr], chdir: "/", output: output, error: error
         if status.success?
-          context.response.respond_with_status status: 200, message: output.to_s
+          context.response.respond_with_status status: 200, message: "packet sent"
+          context.response.output << output.to_s
+          context.response.output << '\n'
+          context.response.close
         else
-          context.response.respond_with_status status: 500, message: {output: output.to_s, error: error.to_s}.to_json
+          context.response.respond_with_status status: 500, message: "etherwake failed"
+          context.response.output << {output: output.to_s, error: error.to_s}.to_pretty_json
+          context.response.output << '\n'
+          context.response.close
         end
       else
-        context.response.respond_with_status status: 500, message: "#{context.request.path}: invalid device name"
+        context.response.respond_with_status status: 500, message: "invalid device"
+        context.response.output.puts "#{context.request.path}: invalid device name"
+        context.response.close
       end
     else
-      context.response.respond_with_status status: 500, message: "#{context.request.path}: invalid request path"
+      context.response.respond_with_status status: 500, message: "invalid path"
+      context.response.output.puts "#{context.request.path}: invalid request path"
+      context.response.close
     end
   end
 end

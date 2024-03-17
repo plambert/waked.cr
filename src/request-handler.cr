2b3d1a4324e6236e235884fb7e81f911360a5eaf
Paul M. Lambert <plambert@plambert.net>
2024-03-17 12:25:49 -0700

    improved output handling once again

--- src/request-handler.cr
+++ src/request-handler.cr
@@ -22,25 +22,25 @@ class Wake::RequestHandler
           context.response.status = 200
           context.response.status_message = "packet sent"
           # context.response.respond_with_status status: 200, message: "packet sent"
-          context.response.close
+          # context.response.close
         else
           context.response.puts({output: output.to_s, error: error.to_s}.to_pretty_json)
           context.response.status = 500
           context.response.status_message = "etherwake failed"
           # context.response.respond_with_status status: 500, message: "etherwake failed"
-          context.response.close
+          # context.response.close
         end
       else
         context.response.output.puts "#{context.request.path}: invalid device name"
         context.response.status = 500
         context.response.status_message = "invalid device"
         # context.response.respond_with_status status: 500, message: "invalid device"
-        context.response.close
+        # context.response.close
       end
     else
       context.response.output.puts "#{context.request.path}: invalid request path"
       context.response.respond_with_status status: 500, message: "invalid path"
-      context.response.close
+      # context.response.close
     end
   end
 end
